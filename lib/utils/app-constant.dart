0259e7394040cd356e1bde61fbf3c25bc234f036
anu5priya7 <anu5priya7@gmail.com>
2023-12-01 19:41:38 +0530

    App Completed

---
+++ lib/utils/app-constant.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class AppConstant {
+  static String appMainName = 'Easy Shopping';
+  static String appPoweredBy = 'Powered By AnuPriya';
+  static const appMainColor = Color(0xff943925);
+  static const appScendoryColor = Color(0xFF8D3D2B);
+  static const appTextColor = Color(0xFFFBF5F4);
+  static const appStatusBarColor = Color(0xFFFBF5F4);
+}
